036285efc9c6a136072f49279910954d4b362e0d
LavanyaN-28 <152402758+LavanyaN-28@users.noreply.github.com>
2024-02-25 22:32:08 +0530

    Created a javascript file ExpenseItem.js
    Created a javascript file
    Adding expense  items using <div> element underneath the Expense Item Header

---
+++ ExpenseItem.js
@@ -0,0 +1,19 @@
+function ExpenseItem(){
+
+    return (
+      <div id="ExpenseItem">
+        <h2>Expense Item </h2>
+        <div>
+          <p>
+            Food Rs 10<br></br>
+            Petrol Rs 100<br></br>
+            Movies Rs 200
+          </p>
+        </div>
+      </div>
+    );
+}
+
+
+
+export default ExpenseItem;
