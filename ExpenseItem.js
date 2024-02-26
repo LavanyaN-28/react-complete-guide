295d3820adec8e09675513ea3a41a080823eebf3
LavanyaN-28 <152402758+LavanyaN-28@users.noreply.github.com>
2024-02-26 21:59:14 +0530

    Modifying App.js and ExpenseItem.js
    Modifying ExpenseItem.js to pass expense items as props and displaying the same on the screen
    Calling ExpenseItems Component 
    Modifying App.js to loop through the expense array dynamically and display the expense items on the screen.

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,14 +1,14 @@
 import './ExpenseItem.css';
 
-function ExpenseItem(){
+function ExpenseItem(props){
 
     return (
       <div className='expense-item'>
-        <div>March 28th 2023</div>
+        <div>{props.date.toISOString()}</div>
         <div className='expense-item__description'>
-            <h2>Car Insurance</h2>
-            <div className='location-of-expenditure'>Bengaluru</div>
-            <div className='expense-item__price'>Rs 2955.75</div>    
+            <h2>{props.title}</h2>
+            <div className='location-of-expenditure'>{props.locationOfExpenditure}</div>
+            <div className='expense-item__price'>{props.amount}</div>    
         </div>
       </div>
     );
