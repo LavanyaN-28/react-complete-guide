e118faee5285da92f9a202babda2bf03f6336841
LavanyaN-28 <152402758+LavanyaN-28@users.noreply.github.com>
2024-02-27 18:31:09 +0530

    Add files via upload

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,19 +1,25 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
+import Card from '../UI/Card';
+import React from 'react';
 
-function ExpenseItem(props){
+const ExpenseItem= (props) => {
 
     return (
-      <div className='expense-item'>
-        <div>{props.date.toISOString()}</div>
-        <div className='expense-item__description'>
-            <h2>{props.title}</h2>
-            <div className='location-of-expenditure'>{props.locationOfExpenditure}</div>
-            <div className='expense-item__price'>{props.amount}</div>    
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <ExpenseDetails
+            amount={props.amount}
+            locationOfExpenditure={props.locationOfExpenditure}
+            title={props.title}
+          />
         </div>
-      </div>
-    );
+      </Card>
+    )
+    
 }
-
 
 
 export default ExpenseItem;
