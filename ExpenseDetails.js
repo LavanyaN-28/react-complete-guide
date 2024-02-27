66e9f430323e4b5902f1a1c21d332e7de0379f00
LavanyaN-28 <152402758+LavanyaN-28@users.noreply.github.com>
2024-02-27 13:15:35 +0530

    Adding ExpenseDate and ExpenseDetails
    Spitting components into multiple components to make the code more readable
    Moving expense date into ExpenseDate.js file and the details of the expense item(title,amount and location of expenditure) into ExpenseDetails,js
    Styling Expense date

---
+++ ExpenseDetails.js
@@ -0,0 +1,17 @@
+import './ExpenseItem.css';
+
+function ExpenseDetails(props){
+
+    return(
+        <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+          <div className="location-of-expenditure">
+            {props.locationOfExpenditure}
+          </div>
+          <div className="expense-item__price">Rs {props.amount}</div>
+        </div>
+    
+    )
+}
+
+export default ExpenseDetails
