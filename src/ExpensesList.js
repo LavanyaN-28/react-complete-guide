1f8c2e0b843ba5d53c028618b1f49a4cb057e64e
LavanyaN-28 <152402758+LavanyaN-28@users.noreply.github.com>
2024-03-05 21:41:51 +0530

    Conditional rendering
    using if condition to show the user that there are no expenses in the selected year.

---
+++ src/ExpensesList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import './ExpensesList.css';
+
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className='expenses-list__fallback'>Found no expenses.</h2>;
+  }
+
+  if (props.items.length === 1) {
+    return <h2 className='expenses-list__fallback'>Only single expense here.Please add more...</h2>;
+  }
+
+
+  return (
+    <ul className='expenses-list'>
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          locationOfExpenditure={expense.locationOfExpenditure}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ExpensesList;
